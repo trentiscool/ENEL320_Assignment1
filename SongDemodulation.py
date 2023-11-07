f96c6ed494bfed8c4eca5553affe912c4df69a0b
trentiscool <trentsdabomb@gmail.com>
2023-11-07 18:13:35 +1300

    updated song code, sort or working

--- SongDemodulation.py
+++ SongDemodulation.py
@@ -1,53 +1,111 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Oct 12 14:22:20 2023
-
-@author: trent
-"""
 
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal
-import sounddevice as sd
-
-t = np.linspace(0, 107768, 107768)
-fs = 44100
-
-#song_signal = np.loadtxt('mystery_song.txt')
-
-message_freq = 4
-Ac = 1
-u = 0.5
-tone_signal = (u*Ac) * np.cos(2*np.pi*message_freq*t)
-
-carrier_freq = 300e3
-carrier_signal = np.cos(2 * np.pi * carrier_freq * t)
-
-modulated_signal = (Ac + tone_signal) * carrier_signal
-demodulated_signal = modulated_signal * 2 * carrier_signal
-
-#analytic_signal = scipy.signal.hilbert(song_signal*2*carrier_signal)
-#envelope = np.abs(analytic_signal)
-
-cutoff_frequency = 3800  # Adjust this cutoff frequency as needed
-filter_order = 6
-nyquist_frequency = 0.5 * fs
-normalized_cutoff = cutoff_frequency / nyquist_frequency
-b, a = scipy.signal.butter(filter_order, normalized_cutoff, btype='low', analog=False)
-output = scipy.signal.lfilter(b, a, demodulated_signal)
+from scipy.io import wavfile
 
 
 """
-Graphs
+Globals
 """
 
-start_value = 0
-end_value = 1000
-fitted_t = t[(t >= start_value) & (t <= end_value)]
+noise = 0.1
 
-#plt.plot(fitted_t, [:len(fitted_t)], label='signal')
-plt.plot(fitted_t, demodulated_signal[:len(fitted_t)], color='red')
-plt.plot(fitted_t, tone_signal[:len(fitted_t)])
-plt.plot(fitted_t, output[:len(fitted_t)], color='black')
+"""
+# TONE MODULATION
+Ac = 2
+fcarrier = 30e3
+fcutoff = 1
+order = 2
 
-sd.play(output, fs) 
+fmessage = 1500
+Am = 1
+
+# TONE SIGNALS
+t = np.arange(0, 3/fmessage, 0.0001/fmessage)
+tone_message = Am * np.cos(2*np.pi*fmessage*t) 
+"""
+
+# SONG DECODING
+fsamp = 44100
+fmessage = 44100
+Am = 1
+fcarrier = 300e3
+Ac = 2
+fcutoff = 10e3
+order = 2
+
+# Mystery Song Signal
+song = np.loadtxt('mystery_song.txt')
+t = np.arange(0, len(song)) * (1/fmessage)
+
+
+
+"""
+Signal Functions
+"""
+
+def EnvelopeDetector (signal):
+    "Takes envelope of AM signal"
+    return np.abs(scipy.signal.hilbert(signal))
+
+def LowPassFilter(signal, fmessage, fcutoff, order):
+    
+    fnyq = 0.5 * fmessage
+    normalized_cutoff = fcutoff / fnyq
+    b, a = scipy.signal.butter(order, normalized_cutoff)
+    return scipy.signal.lfilter(b, a, signal)
+
+def Transmit(signal, t):
+    "Creates transmitted signal"
+    output = (Ac + signal) * np.cos(2*np.pi*fcarrier*t)
+    return output
+    
+    
+def AGWN(signal, noise):
+    "Addative Gausian White Noise"
+    return signal + np.random.normal(0, noise, len(signal))
+
+
+def Recieve(signal):
+    "Recieves and demodulates signal"
+    # Detects envelope
+    m = EnvelopeDetector(signal)
+    
+    # Filters out high frequency components
+    m = LowPassFilter(m, fmessage, fcutoff, order)
+    
+    # Removes DC component
+    return [value - Ac for value in m]
+
+def main():
+    "Signals"
+    
+    #Transmit
+    transmitted_signal = Transmit(song, t)
+    
+    #Channel
+    noise_signal = AGWN(transmitted_signal, noise)
+    
+    #Recieve
+    Recieved_signal = Recieve(noise_signal)
+    
+    "Plot"
+
+    plt.xlabel('Time')
+    plt.ylabel('Amplitude')
+    plt.grid()
+
+    plt.plot(t, transmitted_signal)
+    #plt.plot(t, noise_signal, color='grey')
+    plt.plot(t, Recieved_signal)
+    #plt.plot(t, tone_message)
+    plt.show()
+    
+    scaled_song = np.int16(song / np.max(np.abs(song)) * (2**16))
+    wavfile.write('song.wav', fsamp, scaled_song)
+
+
+
+main()
+
